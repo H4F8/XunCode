906f7e198a0916558560f74531f17c36b5d3dc1a
Hinderchik <gondonloxlp@gmail.com>
2026-08-22 05:20:32 +0300

    chore(release): v1.2.9

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -167,7 +167,7 @@ class UpdateService {
 
   /// Версия по умолчанию для desktop-сборок; на Android берётся из
   /// PackageManager, поэтому здесь она только запасная.
-  static const _fallbackVersion = '1.2.8';
+  static const _fallbackVersion = '1.2.9';
 
   /// Определить источник установки (Шаг 3.1 ТЗ).
   static Future<InstallPlatform> detectPlatform() async {
